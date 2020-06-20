2bd74b71c78fba36ea21bba38f8c49caecba0ce4
Ko3ch <tomko3ch@gmail.com>
2020-06-20 16:32:16 +0300

    added user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { UserServiceService } from '../user-service.service';
+import { Repository } from '../repository';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  user: User;
+  repos: Repository[];
+
+  constructor(private userService: UserServiceService) {
+    }
+
+  ngOnInit(){
+    this.userService.getUser()
+    this.user = this.userService.user
+    this.userService.getUserRepos()
+    this.repos = this.userService.repos
+    console.log(this.repos)
+    console.log(this.user)
+  }  
+
+}
