ebaf2b27ff68b6379aa249f0b9cb8cef9e812e76
Ko3ch <tomko3ch@gmail.com>
2020-06-21 13:11:27 +0300

    added getUserRepos() function

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,36 +1,42 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { User } from '../user'
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { Repository } from '../repository';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
-
+  
   user: User
+  repos: any
   userName: string
-  apiKey: string  
+
   constructor(private http: HttpClient) {
-    this.apiKey = environment.myApiKey;
     this.userName = 'Ko3ch'
-    this.user = new User(0,'','','','',0,'')
+    this.user = new User(0,0,0,'','','','','',0,'')
+  }
+
+  upDateProfile(username: string){
+    this.userName = username
   }
   
   getUser(){
     
     interface ApiResponse{
-      id: number;
-      login: string;
-      avatar_url: string;
-      repos_url: string;
-      bio: string;
-      public_repos: number;
-      html_url: string;
+      id: number
+      login: string
+      name: string
+      avatar_url: string
+      repos_url: string
+      bio: string
+      public_repos: number
+      html_url: string
+      followers: number
+      following: number
     }
     let promise = new Promise((resolve, reject)=>{
-      this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName + '?access_token=&' + this.apiKey).toPromise().then(response=>{
+      this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName + '?access_token=&' + environment.myApiKey).toPromise().then(response=>{
         this.user.id = response.id
         this.user.userAvatar = response.avatar_url
         this.user.userName = response.login
@@ -38,38 +44,22 @@ export class UserServiceService {
         this.user.bio = response.bio
         this.user.public_repos = response.public_repos
         this.user.url = response.html_url
+        this.user.followers = response.followers
+        this.user.following = response.following
+        this.user.name = response.name
         
         resolve();
       },
       error=>{
-        this.user.userName = 'An errror occured!!'
-        
+        this.user.userName = 'Check username again!!';
         reject(error);
         
+      }),
+      this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName + '/repos?access_token=&' + environment.myApiKey).toPromise().then(response=>{
+        this.repos = response
       })
     })
     return promise;
-  } 
-  repos: Repository[]
-  getUserRepos(){
-    
-    interface ApiResponse{
-      repos: any[]
-    }
-    let promise = new Promise((resolve, reject)=>{
-      this.http.get<ApiResponse>('https://api.github.com/users/'+ this.userName + '/repos?access_token=&' + this.apiKey).toPromise().then(response=>{
-        this.repos = response.repos
-        
-        console.log(this.repos)
-        
-        resolve();
-      },
-      error=>{
-        this.user.userName = 'An errror occured!!'
-        
-        reject(error)
-      })
-    })
-    return promise;
-  }  
+  }   
+  
 }
